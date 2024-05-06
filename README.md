9949a2027eaa540af1b46e994d0b0fd8f22e5522
akashk2002 <64390835+akashk2002@users.noreply.github.com>
2024-05-06 09:35:28 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ExpressJS-Cuvute
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/akashk2002/ExpressJS-Cuvute)
---
+++ nodehello.js
@@ -0,0 +1,9 @@
+const { createServer } = require('node:http');
+
+const server = createServer((req, res) => {
+  res.end('hello');
+});
+
+server.listen(3000, () => {
+  console.log('Server is Running');
+});
