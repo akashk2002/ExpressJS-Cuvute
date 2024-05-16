373890818b1872868eee869c6dfa395f55842685
akashk2002 <kanyadharaakash@gmail.com>
2024-05-16 17:09:59 +0530

    EJS: View Engine / Templete Engine - 2

--- NodeServer2/index.js
+++ NodeServer2/index.js
@@ -5,34 +5,50 @@ const app = express()
 
 app.set('view engine', 'ejs')
 
-app.get('/resume/rohith',(req ,res)=>{
-    res.render('resume',{
-        firstName: 'Rohith',
-        lastName : 'Gupta',
-        title: 'Software Engineer Intern',
-        isGraduated: false,
-        hobbies : ['Swiming', 'Reading', 'Coding']
-    })
-})
+// app.get('/resume/rohith',(req ,res)=>{
+//     res.render('resume',{
+//         firstName: 'Rohith',
+//         lastName : 'Gupta',
+//         title: 'Software Engineer Intern',
+//         isGraduated: false,
+//         hobbies : ['Swiming', 'Reading', 'Coding']
+//     })
+// })
 
-app.get('/resume/akash',(req ,res)=>{
-    res.render('resume',{
-        firstName: 'Akash',
-        lastName : 'Kanyadhara',
-        title: 'DevOps Engineer Intern',
-        isGraduated: true,
-        hobbies : ['Running']
+// app.get('/resume/akash',(req ,res)=>{
+//     res.render('resume',{
+//         firstName: 'Akash',
+//         lastName : 'Kanyadhara',
+//         title: 'DevOps Engineer Intern',
+//         isGraduated: true,
+//         hobbies : ['Running']
 
-    })
-})
+//     })
+// })
 
 // app.get('/', (req,res)=>{
 //     res.send('EJS and Request Parameters')
 // })
 
-// app.get('/user/akash',(req,res)=>{
-//     res.sendFile(__dirname + '/pages/user1.html')
-// })
+app.get('/user/akash',(req,res)=>{
+    res.render('user',{
+        firstName: 'Akash',
+        lastName:'Kanyadhara',
+        avatar:'https://reqres.in/img/faces/7-image.jpg',
+        email:'akash@gmail.com',
+    })
+})
+
+app.get('/user/sai',(req,res)=>{
+    res.render('user',{
+        firstName: 'Sai',
+        lastName:'Kumar',
+        avatar:'https://reqres.in/img/faces/7-image.jpg',
+        email:'sai@gmail.com',
+    })
+})
+
+
 // app.get('/user/kanyadhara',(req,res)=>{
 //     res.sendFile(__dirname + '/pages/user2.html')
 // })
