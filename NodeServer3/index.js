bf689aa7e9e7b4f3e01fac33bb858ef9ba4e3896
akashk2002 <kanyadharaakash@gmail.com>
2024-06-06 12:43:29 +0530

    Query and String Parameters

--- NodeServer3/index.js
+++ NodeServer3/index.js
@@ -23,6 +23,20 @@ app.get('/demo',(req,res)=> {
   res.sendFile(__dirname + '/index.html');
 })
 
+//query parameters
+app.get('/shop',(req,res)=> {
+  console.log(req.query);
+  res.send("Welcome to the Shop");
+})
+
+//string parameters
+app.get('/user/:username',(req,res)=> {
+  console.log(req.params);
+  const p = req.params.username
+  res.send(`Welcome to the ${p}`);
+})
+
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
